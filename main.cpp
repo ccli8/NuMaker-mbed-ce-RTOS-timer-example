e3e47d83013987c6be86a9a95dc4f4a9ab61e22d
ccli8 <ccli8@nuvoton.com>
2018-04-17 15:17:54 +0800

    Update to NuMaker-PFM-M487 V3.0 and Mbed OS 5.8.2

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include "mbed.h"
 #include "rtos.h"
 
-DigitalOut LED[4] = {
-    DigitalOut(PD_8), // rgbled_B
-    DigitalOut(PD_9), // rgbled_R
-    DigitalOut(PA_4), // rgbled_G
-    DigitalOut(PG_0)  // greenled
+DigitalOut LED[3] = {
+    DigitalOut(LED1),
+    DigitalOut(LED2),
+    DigitalOut(LED3)
 };
 
 void blink(void const *n) {
@@ -16,11 +15,9 @@ int main() {
     RtosTimer led_1_timer(blink, osTimerPeriodic, (void *)0);
     RtosTimer led_2_timer(blink, osTimerPeriodic, (void *)1);
     RtosTimer led_3_timer(blink, osTimerPeriodic, (void *)2);
-    RtosTimer led_4_timer(blink, osTimerPeriodic, (void *)3);
         
     led_1_timer.start(2000);
     led_2_timer.start(1000);
     led_3_timer.start(500);
-    led_4_timer.start(250);
     Thread::wait(osWaitForever);
 }
